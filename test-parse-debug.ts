3382715b654d82c575e907bb833b89e2549f1627
Steipete <steipete@kora.dev>
2026-04-06 07:14:34 +0200

    chore: remove debug script

--- test-parse-debug.ts
+++
@@ -1,9 +0,0 @@
-import { parseOplDocument } from "./packages/core/src/opl-parse.ts";
-
-const result = parseOplDocument("Boiling is a process, physical.\nBoiling requires 5min.", "SD");
-if (result.ok) {
-  console.log("OK:", result.value.sentences.length);
-  for (const s of result.value.sentences) console.log(" ", s.kind);
-} else {
-  console.log("FAIL:", JSON.stringify(result.error));
-}
